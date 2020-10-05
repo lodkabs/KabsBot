6c6a34b05d998a1eb46ba7c71c98426218f577b3
Kabs <70097297+lodkabs@users.noreply.github.com>
2020-10-05 23:38:46 +0100

    Post first tutorial

---
+++ README.md
@@ -0,0 +1,5 @@
+# KabsBot
+A small project to apply my Python learning to Discord.
+
+## Description
+Currently going through a Discord bot creation tutorial
---
+++ bot.py
@@ -0,0 +1,67 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+import random
+import datetime
+
+try:
+    os.mkdir('logs')
+except OSError:
+    pass
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hey {member.name}, Kabs would normally say hi but...well, you know what he\'s like.\nAnyway, I\'m a bot in his stead, KabsBot! It\'s nice to meet you! :star_struck:'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+    elif message.content == 'raise-exception':
+        raise discord.DiscordException
+
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open('logs/err_' + str(datetime.date.today()) + '.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
+
+client.run(TOKEN)
